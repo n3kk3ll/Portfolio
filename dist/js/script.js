704205920878c82fd8bc5eeaaa1796b420ddacae
Andrii <example.gmail.com>
2021-11-09 21:42:20 +0200

    made skills block

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
     closeElement = document.querySelector('.menu__close'),
-    overlay = document.querySelector('.menu__overlay');
+    overlay = document.querySelector('.menu__overlay'),
+    counter = document.querySelectorAll('.skills__rating-item-counter'),
+    line = document.querySelectorAll('.skills__rating-item-line span');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
@@ -13,4 +15,8 @@ closeElement.addEventListener('click', () => {
 
 overlay.addEventListener('click', () => {
     menu.classList.remove('active');
+});
+
+counter.forEach( (item, i) => {
+    line[i].style.width = item.innerHTML;
 });
